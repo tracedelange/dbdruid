c99a2b049bd54c2bc2b278209b25db8f5b4689a0
Trace <tracedelange@me.com>
2023-04-21 14:27:18 -0500

    1.2 blog post

--- src/blog-posts/index.js
+++ src/blog-posts/index.js
@@ -2,14 +2,14 @@
 
 export const blogPosts = [
     {
-        id: '1',
-        title: 'New Blog, Reubeen and Secret Project',
-        created_date: 'Feb 1, 2023'
-    },
-    {
         id: '3',
         title: 'ReuBeen v1.1',
         created_date: 'Feb 27, 2023'
+    },
+    {
+        id: '4',
+        title: 'ReuBeen v1.2',
+        created_date: 'Apr 22, 2023'
     }
 
 ]
